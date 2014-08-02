81c0c014c9558e6ba3b473018873d262c6bcd4c3
Michael Swedo <mcswedo@gmail.com>
2014-08-01 21:48:58 -0700

    stubs for the rest of the lessons.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@
   schedule = [
     ['Lesson 1 - GitHub and Unity',	20, new Date('2014-09-25T23:59:59'), '<a href="Setup.html">GitHub and Unity</a>'],
     ['Lesson 2 - Developing in Unity', 	20, new Date('2014-09-30T23:59:59'), '<a href="Unity-1.html">Developing in Unity</a>'],
-    ['Lesson 3 - Tutorials in Unity', 20, new Date('2014-10-14T23:59:59'), '<a href="Tutorials.html">Tutorials In Unity</a>']
+    ['Lesson 3 - Tutorials in Unity', 20, new Date('2014-10-14T23:59:59'), '<a href="Tutorials.html">Tutorials In Unity</a>'],
+    ['Lesson 4 - The Game Design Document', 20, new Date('2014-10-16T23:59:59'), '<a href="GDD.html">The Game Design Document</a>'],
+    ['Lesson 5 - Your Game Pitch', 20, new Date('2014-10-21T23:59:59'), '<a href="Pitch.html">Your Game Pitch</a>'],
+    ['Lesson 6 - Game Design Project', 20, new Date('2014-11-25T23:59:59'), '<a href="Project.html">Game Design Project</a>']
   ];
 
   app.onload = function() {
