f328ef42efb1811428ea9d9146abad8f7748c2e1
Michael Swedo <mcswedo@gmail.com>
2014-09-25 10:34:57 -0700

    Updated Point Totals

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 (function() {
 
   schedule = [
-    ['Lesson 1 - Introductions to GitHub and Unity',	20, new Date('2014-09-30T23:59:59'), '<a href="Introduction.html">Week 1: Introductions to GitHub and Unity</a>'],
-    ['Lesson 2 - Tutorials in Unity', 	20, new Date('2014-10-16T23:59:59'), '<a href="Tutorials.html">Weeks 2 & 3: Tutorials in Unity</a>'],
+    ['Lesson 1 - Introductions to GitHub and Unity', 10, new Date('2014-09-30T23:59:59'), '<a href="Introduction.html">Week 1: Introductions to GitHub and Unity</a>'],
+    ['Lesson 2 - Tutorials in Unity', 10, new Date('2014-10-16T23:59:59'), '<a href="Tutorials.html">Weeks 2 & 3: Tutorials in Unity</a>'],
     ['Lesson 3 - Hello World - Input & Movement, Textures & Assets', 20, new Date('2014-10-23T23:59:59'), '<a href="HelloWorld.html">Week 4: Hello World - Input & Movement, Textures & Assets</a>'],
     ['Lesson 4 - Creating a Game World', 20, new Date('2014-10-30T23:59:59'), '<a href="GameWorld.html">Week 5: Creating a Game World</a>'],
     ['Lesson 5 - Physics, Collisions, and Triggers', 20, new Date('2014-11-07T23:59:59'), '<a href="Physics.html">Week 6: Physics, Collisions, and Triggers</a>'],
-    ['Lesson 6 - Audio & GUI', 20, new Date('2014-11-14T23:59:59'), '<a href="Audio-GUI.html">Week 7: Audio & GUI</a>']
+    ['Lesson 6 - Audio & GUI', 20, new Date('2014-11-13T23:59:59'), '<a href="Audio-GUI.html">Week 7: Audio & GUI</a>']
   ];
 
   app.onload = function() {
